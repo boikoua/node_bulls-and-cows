bf981974f1c806b10316686a7a70a058606dd848
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-04-30 10:31:33 +0300

    task solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,53 @@
 'use strict';
+import readline from 'readline';
+import { generateRandomNumber } from './modules/generateRandomNumber.js';
+import { checkIsValidUserInput } from './modules/checkIsValidUserInput.js';
+import { getBullsAndCows } from './modules/getBullsAndCows.js';
 
-// Write your code here
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const numberToGuess = generateRandomNumber();
+
+function checkAnswer(userInput) {
+  if (!checkIsValidUserInput(userInput)) {
+    throw new Error(
+      'You should type 4 digits in the terminal! Please, correct your answer',
+    );
+  }
+
+  const { bulls, cows } = getBullsAndCows(userInput, numberToGuess);
+
+  return { bulls, cows, isWin: bulls === 4 };
+}
+
+function game() {
+  terminal.question(
+    'Try to guess 4 random digits. Enter your answer? ',
+    (userInput) => {
+      const checkResult = checkAnswer(userInput);
+
+      if (!checkResult) {
+        terminal.write(
+          'You should type only 4 digits, without 0 in start position',
+        );
+
+        game();
+      }
+
+      if (checkResult.isWin) {
+        terminal.write('You are champions!!! You guessed all numbers');
+        terminal.close();
+      } else {
+        terminal.write(
+          `You guessed only ${checkAnswer.bulls} and ${checkAnswer.cows}`,
+        );
+        game();
+      }
+    },
+  );
+}
+
+game();
